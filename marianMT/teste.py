19cac0f6c610d703a7e7bbe48d9060d08abd6782
biaborges <biarborges@hotmail.com>
2025-03-13 23:11:45 -0300

    finetuning marian

--- marianMT/teste.py
+++ marianMT/teste.py
@@ -5,7 +5,6 @@ from transformers import MarianMTModel, MarianTokenizer
 
 # Caminhos dos arquivos
 model_path = "/home/ubuntu/finetuning/marianMT/marianMT_frances_ingles/checkpoint-90"
-
 input_file = os.path.abspath("../poemas/poemas300/test/frances_ingles_test.csv")
 output_file = os.path.abspath("../poemas/poemas300/marianmt/frances_ingles_test_traducao_marianmt.csv")
 
@@ -20,13 +19,21 @@ tokenizer = MarianTokenizer.from_pretrained(model_path)
 # Carregar dados do CSV
 df = pd.read_csv(input_file)
 
-# Traduzir cada poema
+# Traduzir cada poema em lotes
+batch_size = 8  # Defina um tamanho de lote (ajuste conforme sua memória)
 translated_texts = []
-for poem in df["original_poem"]:
-    inputs = tokenizer(poem, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
+
+# Função para processar lotes
+def translate_batch(batch):
+    inputs = tokenizer(batch, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
     translated_ids = model.generate(**inputs)
-    translated_text = tokenizer.decode(translated_ids[0], skip_special_tokens=True)
-    translated_texts.append(translated_text)
+    return [tokenizer.decode(t, skip_special_tokens=True) for t in translated_ids]
+
+# Traduzir em lotes
+for i in range(0, len(df), batch_size):
+    batch = df["original_poem"][i:i+batch_size].tolist()
+    translated_batch = translate_batch(batch)
+    translated_texts.extend(translated_batch)
 
 # Adicionar a coluna com a tradução
 df["translated_by_marian"] = translated_texts
