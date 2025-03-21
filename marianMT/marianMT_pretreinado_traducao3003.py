4e1071a4d65428b142e297b432dab090bd93da5d
biaborges <biarborges@hotmail.com>
2025-03-21 14:55:09 -0300

    pretreinado traducao dos 30

--- marianMT/marianMT_pretreinado_traducao3003.py
+++ marianMT/marianMT_pretreinado_traducao3003.py
@@ -1,4 +1,5 @@
 #esse daqui pareceu melhor nos resultados visuais, traduziu todas as frases apesar de muitas terem repetiçoes, mas traduziu tudo!!! 0 300 nao traduziu tudo, mas teve resultados melhores nas métricas. o 3002 é pra ver o poema sozinho.
+#mas esse aqui é pros 30 pre treinados.
 import torch
 import pandas as pd
 from transformers import MarianMTModel, MarianTokenizer
@@ -8,8 +9,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 
 # Carregar modelo e tokenizer do MarianMT
-#model_name = "/home/ubuntu/finetuning/marianMT/marianMT_frances_ingles/checkpoint-160"
-model_name = "Helsinki-NLP/opus-mt-tc-big-en-fr"  
+#model_name = "/home/ubuntu/finetuning/marianMT/marianMT_portugues_ingles/checkpoint-160"
+model_name = "Helsinki-NLP/opus-mt-tc-big-en-pt"  
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name).to(device)
 
@@ -20,7 +21,7 @@ def traduzir_poema(poema, tokenizer, model, device):
 
     # Traduzir verso por verso
     for verso in versos:
-        texto_com_prefixo = f">>fr<< {verso.strip()}"  # Adicionar prefixo da língua
+        texto_com_prefixo = f">>pt<< {verso.strip()}"  # Adicionar prefixo da língua
         encoded = tokenizer(texto_com_prefixo, return_tensors="pt", truncation=True, padding=True, max_length=512)
         encoded = {key: value.to(device) for key, value in encoded.items()}  # Mover para GPU
 
@@ -33,12 +34,12 @@ def traduzir_poema(poema, tokenizer, model, device):
     return "\n".join(traducao_completa)
 
 # Carregar o CSV com os poemas
-df = pd.read_csv('../poemas/poemas300/test/ingles_frances_test.csv')
+df = pd.read_csv('../poemas/poemas300/test/ingles_portugues_test.csv')
 
 # Adicionar a coluna para as traduções
 df['translated_by_marian'] = df['original_poem'].apply(lambda x: traduzir_poema(x, tokenizer, model, device))
 
 # Salvar o CSV com a tradução
-df.to_csv('../poemas/poemas300/marianmt/ingles_frances_test_pretreinado_marianmt.csv', index=False)
+df.to_csv('../poemas/poemas300/marianmt/ingles_portugues_test_pretreinado_marianmt.csv', index=False)
 
 print("Tradução concluída e salva.")
