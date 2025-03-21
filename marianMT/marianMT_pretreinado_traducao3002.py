b38e659a3947fe24b7a9b608fdf4d570baf12416
biaborges <biarborges@hotmail.com>
2025-03-21 01:39:50 -0300

    pretreinado traducao dos 45

--- marianMT/marianMT_pretreinado_traducao3002.py
+++ marianMT/marianMT_pretreinado_traducao3002.py
@@ -11,22 +11,33 @@ tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name).to(device)
 
 # Poema de teste em francês
-poema_original = """je m’éveillai, c’était la maison natale,
-il faisait nuit, des arbres se pressaient
-de toutes parts autour de notre porte,
-j’étais seul sur le seuil dans le vent froid,
-mais non, nullement seul, car deux grands êtres
-se parlaient au-dessus de moi, à travers moi.
-l’un, derrière, une vieille femme, courbe, mauvaise,
-l’autre debout dehors comme une lampe,
-belle, tenant la coupe qu’on lui offrait,
-buvant avidement de toute sa soif.
-ai-je voulu me moquer, certes non,
-plutôt ai-je poussé un cri d’amour
-mais avec la bizarrerie du désespoir,
-et le poison fut partout dans mes membres,
-cérès moquée brisa qui l’avait aimée.
-ainsi parle aujourd’hui la vie murée dans la vie."""
+poema_original = """
+la poésie ne sert à rien
+quand sonne le réveil, strident, brutal
+et qu’il te faut bouger ton corps et ton courage du lit à la douche, au café insipide,
+au garage, à la route, au bureau inutile
+la poésie ne sert à rien . . .
+quand tu déneiges ton pallier,
+quand on te grille la priorité, quand on te raye ta voiture,
+quand tu renverses ton café quand tu oublies, quand tu rates,
+quand pressent les réunions, quand tombent les deadlines,
+quand tu oublies de passer prendre le pain et les enfants
+la poésie ne sert à rien?
+quand partent les absents
+de nos mémoires vides, de nos images effacées par la distance, le temps et les soucis
+quand leurs visages deviennent flous vagues, pâles ou imprécis
+la poésie, sert-elle à quelque chose?
+quand crache la télé, du monde les infamies les guerres, les morts, les dictatures, les peurs, la faim, la haine, le désespoir, la rage, l’horreur, la peine et les tortures
+la poésie sert à quelque chose
+quand sonne le réveil, sifflant, jovial
+et que tu te sais vivant, présent, debout
+dans ton espace peuplé de gens, de sons, de souvenirs
+la poésie sert
+à savourer ton café, à te foutre
+des dates butoir, médiocrités et autres impératifs
+elle t’entoure dans tes rires, tes pleurs et tes orgasmes
+la poésie est un orgasme!
+"""
 
 # Separar por linhas para evitar truncamento
 versos = poema_original.split("\n")
