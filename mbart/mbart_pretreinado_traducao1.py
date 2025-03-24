da5572c1ffb799bda620ebeffe8bd922e6190c94
biaborges <biarborges@hotmail.com>
2025-03-23 21:13:30 -0300

    traduzir mbart

--- mbart/mbart_pretreinado_traducao1.py
+++ mbart/mbart_pretreinado_traducao1.py
@@ -5,29 +5,36 @@ from transformers import MBartForConditionalGeneration, MBart50TokenizerFast
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 
-# Carregar o tokenizador e o modelo pré-treinado para tradução do francês para o inglês
-model_name = "facebook/mbart-large-50-one-to-many-mmt"
-tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
-model = MBartForConditionalGeneration.from_pretrained(model_name)
+input = """
+je m’éveillai, c’était la maison natale,
+il faisait nuit, des arbres se pressaient
+de toutes parts autour de notre porte,
+j’étais seul sur le seuil dans le vent froid,
+mais non, nullement seul, car deux grands êtres
+se parlaient au-dessus de moi, à travers moi.
+l’un, derrière, une vieille femme, courbe, mauvaise,
+l’autre debout dehors comme une lampe,
+belle, tenant la coupe qu’on lui offrait,
+buvant avidement de toute sa soif.
+ai-je voulu me moquer, certes non,
+plutôt ai-je poussé un cri d’amour
+mais avec la bizarrerie du désespoir,
+et le poison fut partout dans mes membres,
+cérès moquée brisa qui l’avait aimée.
+ainsi parle aujourd’hui la vie murée dans la vie.
+"""
 
-# Definir o idioma de origem (francês)
+model = MBartForConditionalGeneration.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+tokenizer = MBart50TokenizerFast.from_pretrained("facebook/mbart-large-50-many-to-many-mmt")
+
+
 tokenizer.src_lang = "fr_XX"
-
-# Texto em francês para tradução
-texto_frances = "Bonjour, comment ça va ?"
-
-# Tokenizar o texto de entrada
-inputs = tokenizer(texto_frances, return_tensors="pt").to(device)
-
-# Obter o ID do idioma de destino (inglês)
-tgt_lang_id = tokenizer.lang2id["en_XX"]
-
-# Gerar a tradução (usando forced_bos_token_id para garantir que a tradução será para o inglês)
-translated = model.generate(**inputs, forced_bos_token_id=tgt_lang_id).to(device)
-
-# Decodificar a tradução de volta para texto
-translated_text = tokenizer.decode(translated[0], skip_special_tokens=True)
+encoded_text = tokenizer(input, return_tensors="pt")
+generated_tokens = model.generate(
+    **encoded_text,
+    forced_bos_token_id=tokenizer.lang_code_to_id["en_XX"]
+)
+translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
 
 # Exibir a tradução
-print(f"Texto original (francês): {texto_frances}")
 print(f"Tradução (inglês): {translated_text}")
