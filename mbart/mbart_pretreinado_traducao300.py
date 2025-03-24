c4feece94cd12c99f777421d60ee632a3efa9b83
biaborges <biarborges@hotmail.com>
2025-03-24 00:39:57 -0300

    traduzir mbart

--- mbart/mbart_pretreinado_traducao300.py
+++ mbart/mbart_pretreinado_traducao300.py
@@ -9,24 +9,23 @@ start_time = time.time()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Usando dispositivo: {device}")
 
-# Carregar modelo e tokenizer do mBART para francês -> inglês
+# Carregar modelo e tokenizer do mBART
 model_name = "facebook/mbart-large-50-many-to-many-mmt"
 tokenizer = MBart50TokenizerFast.from_pretrained(model_name)
 model = MBartForConditionalGeneration.from_pretrained(model_name).to(device)
 
 # Função para traduzir poema
 def traduzir_poema(poema, src_lang="fr_XX", tgt_lang="pt_XX"):
-    # Configurar a língua de origem e destino no tokenizer
-    tokenizer.src_lang = src_lang
-    tokenizer.tgt_lang = tgt_lang
-
-    # Tokenizar o verso
-    encoded = tokenizer(poema.strip(), return_tensors="pt", truncation=True, padding=True, max_length=512)
+    # Adicionar prefixo da língua de destino ao texto de entrada
+    input_text = f" >>{tgt_lang}<< {poema.strip()}"
+    
+    # Tokenizar o texto com a configuração correta
+    encoded = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True, max_length=512)
     encoded = {key: value.to(device) for key, value in encoded.items()}  # Mover para GPU
 
     # Gerar tradução
     with torch.no_grad():
-        generated_tokens = model.generate(**encoded, max_length=512, num_beams=5)
+        generated_tokens = model.generate(**encoded, max_length=512, num_beams=5, forced_bos_token_id=tokenizer.lang_code_to_id[tgt_lang])
 
     traducao = tokenizer.decode(generated_tokens[0], skip_special_tokens=True)
     return traducao
@@ -36,7 +35,7 @@ file_path = "../poemas/poemas300/test/frances_portugues_test.csv"  # Altere o ca
 df = pd.read_csv(file_path)
 
 # Traduzir os poemas e adicionar à nova coluna 'translated_by_TA'
-df['translated_by_TA'] = df['original_poem'].apply(lambda poem: traduzir_poema(poem, src_lang="fr_XX", tgt_lang="pt_XX"))
+df['translated_by_TA'] = df.apply(lambda row: traduzir_poema(row['original_poem'], src_lang=row['src_lang'], tgt_lang=row['tgt_lang']), axis=1)
 
 # Salvar o resultado em um novo CSV
 df.to_csv("../poemas/poemas300/mbart/frances_portugues_test_pretreinado_mbart.csv", index=False)
