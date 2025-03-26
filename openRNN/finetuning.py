c0b7c23a4c1479b14ba1fd5f36ab9903f87ad353
biaborges <biarborges@hotmail.com>
2025-03-26 16:45:05 -0300

    openrnn

--- openRNN/finetuning.py
+++ openRNN/finetuning.py
@@ -94,6 +94,7 @@ tgt_vocab: output_data/vocab.tgt
 
 model_dir: output_model
 save_model: output_model/model
+save_data: output_data/onmt
 save_checkpoint_steps: 1000
 train_steps: 50000
 valid_steps: 1000
