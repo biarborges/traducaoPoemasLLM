dd67471dbcf20101f5c53a6f9c14e6479752a510
biaborges <biarborges@hotmail.com>
2025-03-27 11:15:19 -0300

    openrnn

--- openRNN/traducao_finetuning.py
+++ openRNN/traducao_finetuning.py
@@ -6,12 +6,12 @@ start_time = time.time()
 
 # Configuração dos modelos de tradução
 MODEL_PATHS = {
-    ("fr_XX", "en_XX"): "../openRNN/models_fr_en/model_fr_en_step_50000.pt"
+    ("fr_XX", "en_XX"): "../openRNN/models_fr_en/model_fr_en_step_10000.pt"
     # Adicione mais idiomas se necessário
 }
 
-CSV_PATH = "../poemas/poemas300/frances_ingles_poems.csv"
-OUTPUT_CSV = "../poemas/poemas300/openRNN/frances_ingles_poems_openRNN.csv"
+CSV_PATH = "../poemas/poemas300/test/frances_ingles_test.csv"
+OUTPUT_CSV = "../poemas/poemas300/openRNN/frances_ingles_test_finetuning_openRNN.csv"
 
 def traduzir_texto(texto, src, tgt):
     """Traduz um texto usando o modelo adequado"""
