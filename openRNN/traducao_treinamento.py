1f3161da62749f24cf53753138c1c160219f0982
biaborges <biarborges@hotmail.com>
2025-03-28 18:42:21 -0300

    openRNN

--- openRNN/traducao_treinamento.py
+++ openRNN/traducao_treinamento.py
@@ -6,7 +6,7 @@ start_time = time.time()
 
 # Configuração dos modelos de tradução
 MODEL_PATHS = {
-    ("fr_XX", "en_XX"): "../openRNN/models_en_fr/model_en_fr_step_50000.pt"
+    ("en_XX", "fr_XX"): "../openRNN/models_en_fr/model_en_fr_step_50000.pt"
     # Adicione mais idiomas se necessário
 }
 
