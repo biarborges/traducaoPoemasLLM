8854129e384dc26802d09a6a83607022d8172420
biaborges <biarborges@hotmail.com>
2025-04-09 19:36:49 -0300

    opennmt

--- openNMT/traducao_pretreinado.py
+++ openNMT/traducao_pretreinado.py
@@ -4,7 +4,7 @@ import os
 from tqdm import tqdm
 
 # Caminhos
-CSV_PATH = "../poemas/ingles_frances_poems_teste.csv"
+CSV_PATH = "../poemas/frances_ingles_poems_teste.csv"
 OUT_DIR = "../poemas/OpenNMT/frances_ingles_poems_OpenNMT.csv"
 MODEL_PATH = "nllb-200-1.3B-onmt.pt"
 SP_MODEL_PATH = "flores200_sacrebleu_tokenizer_spm.model"
