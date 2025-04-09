8847f65bb6a13574a546340d96694b63808e71b0
biaborges <biarborges@hotmail.com>
2025-04-09 20:30:35 -0300

    opennmt

--- openNMT/traducao_pretreinado.py
+++ openNMT/traducao_pretreinado.py
@@ -6,7 +6,7 @@ import time
 start_time = time.time()
 
 # Caminhos
-CSV_PATH = "../poemas/frances_ingles_poems_teste.csv"
+CSV_PATH = "../poemas/frances_ingles_poems.csv"
 OUTPUT_CSV = "../poemas/openNMT/frances_ingles_poems_opennmt.csv"
 TEMP_INPUT = "../poemas/openNMT/temp_input.txt"
 TEMP_OUTPUT = "../poemas/openNMT/output.txt"
