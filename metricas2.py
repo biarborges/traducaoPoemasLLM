8e986f9bc875fc61717c3c42b9762d005a121737
biaborges <biarborges@hotmail.com>
2025-04-10 05:08:52 -0300

    opennmt

--- metricas2.py
+++ metricas2.py
@@ -17,7 +17,7 @@ except LookupError:
 
 
 
-input_file = os.path.abspath("../TraducaoPoemasLLM/poemas/poemas300/marianmt/ingles_frances_test_finetuning_marianmt_3003.csv")
+input_file = os.path.abspath("../poemas/marianmt/frances_ingles_test_finetuning_marianmt.csv")
 
 #BLEU
 
