5540023db2ace84f17f8ac7bcf4b95e43cb058b0
biaborges <biarborges@hotmail.com>
2025-06-09 23:33:09 -0300

    modelagem de topicos

--- modelagemTopicos/bertopic3.py
+++ modelagemTopicos/bertopic3.py
@@ -63,12 +63,12 @@ print("Gerando embeddings...")
 embeddings = embedding_model.encode(poemas_limpos, show_progress_bar=True)
 
 print("Treinando modelo BERTopic...")
-# Ajuste: min_cluster_size pequeno → mais tópicos
-#hdbscan_model = HDBSCAN(min_cluster_size=5, min_samples=2, metric='euclidean', prediction_data=True)
+
+hdbscan_model = HDBSCAN(min_cluster_size=5, min_samples=2, metric='euclidean', prediction_data=True)
 
 # Usa o modelo HDBSCAN no BERTopic
-#topic_model = BERTopic(language="multilingual", hdbscan_model=hdbscan_model, nr_topics=10)
-topic_model = BERTopic(language="multilingual")
+topic_model = BERTopic(language="multilingual", hdbscan_model=hdbscan_model, nr_topics=10)
+#topic_model = BERTopic(language="multilingual")
 topics, probs = topic_model.fit_transform(poemas_limpos, embeddings)
 
 
