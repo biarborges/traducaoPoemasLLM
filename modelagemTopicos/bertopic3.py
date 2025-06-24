30bd893a445237b7da9a11b370df9db8261856d8
biaborges <biarborges@hotmail.com>
2025-06-24 17:38:38 -0300

    bertopic score e topicos

--- modelagemTopicos/bertopic3.py
+++ modelagemTopicos/bertopic3.py
@@ -24,11 +24,11 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 SEED = 38
 
-TITLE = "chatGPTPrompt2"
+TITLE = "googleTradutor"
 # original reference chatGPTPrompt1 googleTradutor maritacaPrompt1
 
 # Caminho para o arquivo de entrada
-CAMINHO_CSV = "poemas_unificados_chatGPTPrompt2.csv"
+CAMINHO_CSV = "poemas_unificados_googleTradutor.csv"
 # chatGPTPrompt1 googleTradutor maritacaPrompt1
 
 # Pasta para salvar os resultados
@@ -38,7 +38,7 @@ PASTA_SAIDA = "results"
 COLUNA_POEMAS = "translated_by_TA"  # "original_poem", "translated_poem", "translated_by_TA"
 
 # Definição dos idiomas de origem e destino para filtrar o CSV
-IDIOMA_ORIGEM = "pt_XX"  #  "fr_XX", "pt_XX", "en_XX"
+IDIOMA_ORIGEM = "fr_XX"  #  "fr_XX", "pt_XX", "en_XX"
 IDIOMA_DESTINO = "en_XX" #  "fr_XX", "pt_XX", "en_XX"
 
 # Idioma para o pré-processamento (NLTK e spaCy)
