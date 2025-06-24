7a5a2df49c81942bcbb54728f915584893cbfe41
biaborges <biarborges@hotmail.com>
2025-06-24 17:18:46 -0300

    bertopic score e topicos

--- modelagemTopicos/bertopic3.py
+++ modelagemTopicos/bertopic3.py
@@ -22,7 +22,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # 1. CONFIGURAÇÕES E CONSTANTES
 # ==============================================================================
 
-SEED = 40
+SEED = 42
 
 TITLE = "chatGPTPrompt2"
 # original reference chatGPTPrompt1 googleTradutor maritacaPrompt1
@@ -38,11 +38,11 @@ PASTA_SAIDA = "results"
 COLUNA_POEMAS = "translated_by_TA"  # "original_poem", "translated_poem", "translated_by_TA"
 
 # Definição dos idiomas de origem e destino para filtrar o CSV
-IDIOMA_ORIGEM = "fr_XX"  #  "fr_XX", "pt_XX", "en_XX"
-IDIOMA_DESTINO = "pt_XX" #  "fr_XX", "pt_XX", "en_XX"
+IDIOMA_ORIGEM = "en_XX"  #  "fr_XX", "pt_XX", "en_XX"
+IDIOMA_DESTINO = "fr_XX" #  "fr_XX", "pt_XX", "en_XX"
 
 # Idioma para o pré-processamento (NLTK e spaCy)
-IDIOMA_PROC = "pt_XX"
+IDIOMA_PROC = "fr_XX"
 
 
 
